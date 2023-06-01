5c95eafb6d32b9a2c30f5e7b512c22220b503de8
kyumin1227 <68456336+kyumin1227@users.noreply.github.com>
2023-06-01 19:15:05 +0900

    Modal test

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// 모달 내부 댓글 작성
+const modalCommentSubmit = document.querySelector("#modal_submitBtn");
+const modalCommentText = document.querySelector("#modal_commentText");
+
+const handleModalComment = () => {
+  let formData = new FormData();
+  formData.append();
+  formData.append();
+};
+
+modalCommentSubmit.addEventListener("click", handleModalComment);
