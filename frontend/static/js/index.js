909cb46e1ea08b8fc390bac7356cae760ba4f8a2
kyumin1227 <68456336+kyumin1227@users.noreply.github.com>
2023-06-03 15:57:28 +0900

    Spa server

---
+++ frontend/static/js/index.js
@@ -0,0 +1,47 @@
+import Home from "./pages/Home.js";
+import Posts from "./pages/Posts.js";
+import Settings from "./pages/Settings.js";
+
+const router = async () => {
+  const routes = [
+    { path: "/", view: Home },
+    { path: "/posts", view: Posts },
+    { path: "/settings", view: Settings },
+  ];
+
+  const pageMatches = routes.map((route) => {
+    return {
+      route, // route: route
+      isMatch: route.path === location.pathname,
+    };
+  });
+
+  let match = pageMatches.find((pageMatch) => pageMatch.isMatch);
+
+  if (!match) {
+    match = {
+      route: location.pathname,
+      isMatch: true,
+    };
+    const page = new NotFound();
+    document.querySelector("#root").innerHTML = await page.getHtml();
+  } else {
+    const page = new match.route.view();
+    document.querySelector("#root").innerHTML = await page.getHtml();
+  }
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.body.addEventListener("click", (e) => {
+    if (e.target.matches("[data-link]")) {
+      e.preventDefault();
+      history.pushState(null, null, e.target.href);
+      router();
+    }
+  });
+  router();
+});
+
+window.addEventListener("popstate", () => {
+  router();
+});
