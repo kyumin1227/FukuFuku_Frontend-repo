2f852ee62f2924eb5bc5adf628042a574988edd6
kyumin1227 <68456336+kyumin1227@users.noreply.github.com>
2023-06-08 02:09:11 +0900

    Docs : 엘리먼트 id 변경
    - BulletinModal.js : 모달 id 변경하였습니다.
    - BulletinPage.html : 이름 변경 하였습니다.
    - BulletinPage.js : 게시글 블록의 부모 엘리먼트에 board_list 아이디 추가하였습니다.

--- frontend/static/js/BulletinModal.js
+++ frontend/static/js/BulletinModal.js
@@ -19,100 +19,100 @@ let comments = "";
 
 let modals = "";
 
-modals += `<div class="modal" id="${modalId}">  <!-- modal 아이디 지정 -->
-        <div class="modal-dialog modal-xl">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h1 class="modal-title fs-5">${title}</h1>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <div class="container-fluid">
-                        <div class="row">
-                            <div class="col-8 p-0">
-                                <!-- 이미지 창 -->
+modals += `<div class="modal" id="${modalId}"> <!-- modal 아이디 지정 -->
+    <div class="modal-dialog modal-xl">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5">${title}</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-8 p-0">
+                            <!-- 이미지 창 -->
 
-                                <div class="d-flex align-items-center">
+                            <div class="d-flex align-items-center">
 
-                                    <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="true">
-                                        <div class="carousel-indicators">
-                                            <button type="button" data-bs-target="#carouselExampleIndicators"
-                                                data-bs-slide-to="0" class="active" aria-current="true"
-                                                aria-label="Slide 1"></button>
-                                            <button type="button" data-bs-target="#carouselExampleIndicators"
-                                                data-bs-slide-to="1" aria-label="Slide 2"></button>
-                                            <button type="button" data-bs-target="#carouselExampleIndicators"
-                                                data-bs-slide-to="2" aria-label="Slide 3"></button>
-                                        </div>
-                                        <div class="carousel-inner">
-                                            <div class="carousel-item active">
-                                                <img src="${imgUrl}" class="d-block w-100" alt="...">
-                                            </div>
-                                            <div class="carousel-item">
-                                                <img src="./static/image/미야케 우동2.jpg" class="d-block w-100" alt="...">
-                                            </div>
-                                            <div class="carousel-item">
-                                                <img src="./static/image/미야케 우동3.jpg" class="d-block w-100" alt="...">
-                                            </div>
-                                        </div>
-                                        <button class="carousel-control-prev" type="button"
-                                            data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-                                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                                            <span class="visually-hidden">Previous</span>
-                                        </button>
-                                        <button class="carousel-control-next" type="button"
-                                            data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-                                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                                            <span class="visually-hidden">Next</span>
-                                        </button>
+                                <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="true">
+                                    <div class="carousel-indicators">
+                                        <button type="button" data-bs-target="#carouselExampleIndicators"
+                                            data-bs-slide-to="0" class="active" aria-current="true"
+                                            aria-label="Slide 1"></button>
+                                        <button type="button" data-bs-target="#carouselExampleIndicators"
+                                            data-bs-slide-to="1" aria-label="Slide 2"></button>
+                                        <button type="button" data-bs-target="#carouselExampleIndicators"
+                                            data-bs-slide-to="2" aria-label="Slide 3"></button>
                                     </div>
-                                </div>
-                            </div>
-                            <div class="col-4 d-flex justify-content-center" style="overflow-y: auto; height: 65vh;">
-                                <!-- 댓글 창 -->
-                                <div id="${boardId}block_comment" class="col-11">   <!-- 모달의 코멘트 블록 아이디 지정 -->
-                                    <span class="mt-5 fs-4">Comment</span>
-                                    
-                                    ${comments}
-                                    
+                                    <div class="carousel-inner">
+                                        <div class="carousel-item active">
+                                            <img src="${imgUrl}" class="d-block w-100" alt="...">
+                                        </div>
+                                        <div class="carousel-item">
+                                            <img src="./static/image/미야케 우동2.jpg" class="d-block w-100" alt="...">
+                                        </div>
+                                        <div class="carousel-item">
+                                            <img src="./static/image/미야케 우동3.jpg" class="d-block w-100" alt="...">
+                                        </div>
+                                    </div>
+                                    <button class="carousel-control-prev" type="button"
+                                        data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                        <span class="visually-hidden">Previous</span>
+                                    </button>
+                                    <button class="carousel-control-next" type="button"
+                                        data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                        <span class="visually-hidden">Next</span>
+                                    </button>
                                 </div>
                             </div>
                         </div>
+                        <div class="col-4 d-flex justify-content-center" style="overflow-y: auto; height: 65vh;">
+                            <!-- 댓글 창 -->
+                            <div id="${boardId}block_comment" class="col-11"> <!-- 모달의 코멘트 블록 아이디 지정 -->
+                                <span class="mt-5 fs-4">Comment</span>
 
-                        <!-- 본문, 댓글작성 창 -->
-                        <div class="row">
+                                ${comments}
 
-                            <!-- 본문 창 -->
-
-                            <div class="col-8 bg-secondary bg-opacity-50 rounded-1 d-flex justify-content-between flex-column p-3"
-                                style="height: 15vh;">
-                                <span>${content}</span>
-                                <div class="row d-flex justify-content-end">
-                                    <div class="col-6">작성 일자: <span>${writeDate}</span></div>
-                                    <div class="col-3">작성자: <span>${writer}</span></div>
-                                    <div class="col-3">조회수: <span>${hit}</span></div>
-                                </div>
                             </div>
+                        </div>
+                    </div>
 
-                            <!-- 댓글작성 창 -->
+                    <!-- 본문, 댓글작성 창 -->
+                    <div class="row">
 
-                            <div class="col-4">
-                                <div class="row mt-3 h-75">
-                                    <div class="col-12 d-flex justify-content-end">
-                                        <textarea id="modal_commentText" class="form-control"></textarea>
-                                        <button id="modal_submitBtn" class="btn btn-primary">작성</button>
-                                    </div>
+                        <!-- 본문 창 -->
+
+                        <div class="col-8 bg-secondary bg-opacity-50 rounded-1 d-flex justify-content-between flex-column p-3"
+                            style="height: 15vh;">
+                            <span>${content}</span>
+                            <div class="row d-flex justify-content-end">
+                                <div class="col-6">작성 일자: <span>${writeDate}</span></div>
+                                <div class="col-3">작성자: <span>${writer}</span></div>
+                                <div class="col-3">조회수: <span>${hit}</span></div>
+                            </div>
+                        </div>
+
+                        <!-- 댓글작성 창 -->
+
+                        <div class="col-4">
+                            <div class="row mt-3 h-75">
+                                <div class="col-12 d-flex justify-content-end">
+                                    <textarea id="modal_commentText${boardNo}" class="form-control"></textarea>
+                                    <button id="modal_submitBtn${boardNo}" class="btn btn-primary">작성</button>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Save changes</button>
-                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
             </div>
         </div>
-    </div>`;
+    </div>
+</div>`;
 
 export default modals;
